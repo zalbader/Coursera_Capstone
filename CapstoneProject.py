3f83c573581124529da923597bc822fc7b7124a0
zalbader <54176120+zalbader@users.noreply.github.com>
2019-08-17 00:37:17 +0300

    Add files via upload

---
+++ CapstoneProject.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+# This notebook will be mainly used for the capstone project
+
+
+# In[2]:
+
+
+import pandas as pd
+import numpy as np
+
+
+# In[3]:
+
+
+print("Hello Capstone Project Course!")
+
+
+# In[ ]:
+
+
+
+
